a9c8d8516ab48ee8b9e2f5b4f3abdfea497d8299
GeorgiosDiamantakis <128605381+GeorgiosDiamantakis@users.noreply.github.com>
2023-07-18 19:59:47 +0300

    Add files via upload

---
+++ Flight Booking App/script.js
@@ -0,0 +1,25 @@
+const nameOnPass = "Georgios";
+console.log("Passenger Name: " + nameOnPass);
+const nameOnId = "Georgios";
+const nameMatch = nameOnPass === nameOnId;
+console.log("ID confirmed? " + nameMatch);
+const length = 29;
+const doesLengthPass = length < 30;
+const height = 54;
+const doesHeightPass = height < 55;
+const isHandBaggageEligible = doesLengthPass && doesHeightPass;
+console.log("Is Hand Baggage Eligible? " + isHandBaggageEligible);
+const flightDes = "Athens";
+const returnFrom = "Paris";
+const isRoundTrip = flightDes === returnFrom;
+console.log("Round Trip Check?" + isRoundTrip);
+const bookings = 12;
+const totalCapacity = 52;
+const isFlightAvailable = bookings < totalCapacity;
+console.log("are seats available? " + isFlightAvailable);
+
+document.querySelector("#name").innerHTML = nameOnPass;
+document.querySelector("#id").innerHTML = nameMatch;
+document.querySelector("#bag").innerHTML = isHandBaggageEligible;
+document.querySelector("#des").innerHTML = isRoundTrip;
+document.querySelector("#seat").innerHTML = isFlightAvailable;
